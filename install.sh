e7ab8c2480d452d9dc9360134bd97730ff9a1c09
Miroslav Safr <Miroslav.Safr@gmail.com>
2012-02-29 14:26:33 +0200

    Chg: install.sh explicit file names

--- install.sh
+++ install.sh
@@ -5,4 +5,10 @@
 BINDIR=/usr/bin/
 
 sudo mkdir -p -m 0755 $BINDIR
-sudo install -m 0777 -v ./gr-*  $BINDIR
+sudo install -m 0777 -v ./gr-branches $BINDIR
+sudo install -m 0777 -v ./gr-clean $BINDIR
+sudo install -m 0777 -v ./gr-pull $BINDIR
+sudo install -m 0777 -v ./gr-pullreset $BINDIR
+sudo install -m 0777 -v ./gr-remotes $BINDIR
+sudo install -m 0777 -v ./gr-show $BINDIR
+sudo install -m 0777 -v ./gr-showlocal $BINDIR
