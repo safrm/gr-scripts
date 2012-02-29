49213bcd2bb51b7d0d2da1c010ce7b54a1c991b8
Miroslav Safr <Miroslav.Safr@gmail.com>
2012-02-29 12:14:54 +0200

    New: -v options for gr-show and gr-showlocal

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 #git recursive directories scripts - https://github.com/safrm/gr-scripts
 #version: 1.0
 #author:  Miroslav Safr miroslav.safr@gmail.com
-BINDIR=/usr/local/bin/
+BINDIR=/usr/bin/
 
 sudo mkdir -p -m 0755 $BINDIR
 sudo install -m 0777 -v ./gr-*  $BINDIR
