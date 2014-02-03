953bf8e153f3b6a5de5c0681437d59ae4e5a6084
Miroslav Safr <Miroslav.Safr@gmail.com>
2014-02-03 10:19:59 +0200

    New: Finished man pages, update script, utf8 encoding
    Signed-off-by: Miroslav Safr <Miroslav.Safr@gmail.com>

--- doc/update_docs.sh
+++ doc/update_docs.sh
@@ -3,7 +3,7 @@
 #author:  Miroslav Safr miroslav.safr@gmail.com
 
 
-XSLTPROC=/usr/bin/xsltproc
+XSLTPROC=/usr/bin/xsltproc 
 if [ ! -e $MANXSL ]; then
 	echo "$XSLTPROC does not exist, exiting.."
 	exit 1
@@ -19,10 +19,24 @@ if [ ! -e $MANXSL ]; then
 	MANXSL=http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl
 fi
 
-#generate man and html pages
+#automatic version 
+if command -v appver &>/dev/null; then . appver; else APP_SHORT_VERSION=NA ; APP_FULL_VERSION_TAG=NA ; APP_BUILD_DATE=`date +'%Y%m%d_%H%M'`; fi
+
+TEMP_DIR=./tmp
+rm -fr $TEMP_DIR && mkdir $TEMP_DIR
+#update version and date
 for PAGEXML in $(  find . -type f -name "*8.xml" )
 do
-	PAGENAME=`basename -s.8.xml $PAGEXML`
+    cp $PAGEXML $TEMP_DIR/
+    sed -i".bkp" "1,/<productnumber>/s/<productnumber>.*/<productnumber>$APP_SHORT_VERSION_TAG<\/productnumber>/" $TEMP_DIR/$PAGEXML  && rm -f $TEMP_DIR/$PAGEXML.bkp
+    sed -i".bkp" "1,/<\/date>/s/<date>.*/<date>$APP_BUILD_DATE<\/date>/" $TEMP_DIR/$PAGEXML  && rm -f $TEMP_DIR/$PAGEXML.bkp
+done
+
+#generate man and html pages
+for PAGEXML in $(  find ./tmp -type f -name "*8.xml" )
+do
+#	PAGENAME=`basename -s.8.xml $PAGEXML`
+	PAGENAME=`basename $PAGEXML .8.xml`
 	$XSLTPROC -o ./htmlpages/$PAGENAME.html $HTMLXSL $PAGEXML
 	$XSLTPROC -o ./manpages/$PAGENAME.8 $MANXSL $PAGEXML
 done
