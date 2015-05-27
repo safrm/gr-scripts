bf54fcdf7a8d179db80d8aeecf35b4206b141b6b
Miroslav Safr <Miroslav.Safr@gmail.com>
2015-05-27 22:52:27 +0300

    New: install supports msys
    Signed-off-by: Miroslav Safr <Miroslav.Safr@gmail.com>

--- install.sh
+++ install.sh
@@ -7,70 +7,74 @@ COMPLETION_DIR=/etc/bash_completion.d
 MANDIR=/usr/share/man
 
 #root check
-USERID=`id -u`
-[ $USERID -eq "0" ] || {
+[ `id -u` -ne 0 ] && [ `uname -s | grep -i linux` ] && {
     echo "I cannot continue, you should be root or run it with sudo!"
     exit 0
 }
+if command -v install 1>/dev/null 2>&1 ; then
+    INSTALL_755="install -m 0755 -v " ; INSTALL_644="install -m 0644 -v "
+else
+    INSTALL_755="cp -afv " ; INSTALL_644="cp -afv "
+fi
 
 #automatic version
-if command -v appver 1>/dev/null 2>&1 ; then . appver; else APP_SHORT_VERSION=NA ; APP_FULL_VERSION_TAG=NA ; APP_BUILD_DATE=`date +'%Y%m%d_%H%M'`; fi
+if command -v appver 1>/dev/null 2>&1 ; then . appver ; else APP_SHORT_VERSION=NA ; APP_FULL_VERSION_TAG=NA ; APP_BUILD_DATE=`date +'%Y%m%d_%H%M'` ; fi
 
 #test
 for TEST in $(  grep -r -l -h "#\!/bin/sh"  --exclude-dir=.git . )
 do
-		sh -n $TEST
-		if  [ $? != 0 ]; then
-			echo "syntax error in $TEST, exiting.." 
-			exit 1
-		fi
+    sh -n $TEST
+    if  [ $? != 0 ]; then
+        echo "syntax error in $TEST, exiting.."
+        exit 1
+	fi
 done
 
 #update documentation
 jss-docs-update ./doc 
 
 mkdir -p -m 0755 $BINDIR
-install -m 0777 -v ./gr-authorcheck $BINDIR
+$INSTALL_755 ./gr-authorcheck $BINDIR
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/gr-authorcheck  && rm -f $BINDIR/gr-authorcheck.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/gr-authorcheck  && rm -f $BINDIR/gr-authorcheck.bkp
-install -m 0777 -v ./gr-branches $BINDIR
+$INSTALL_755 ./gr-branches $BINDIR
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/gr-branches && rm -f $BINDIR/gr-branches.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/gr-branches && rm -f $BINDIR/gr-branches.bkp
-install -m 0777 -v ./gr-clean $BINDIR
+$INSTALL_755 ./gr-clean $BINDIR
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/gr-clean && rm -f $BINDIR/gr-clean.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/gr-clean && rm -f $BINDIR/gr-clean.bkp
-install -m 0777 -v ./gr-commits2tag $BINDIR
+$INSTALL_755 ./gr-commits2tag $BINDIR
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/gr-commits2tag && rm -f $BINDIR/gr-commits2tag.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/gr-commits2tag && rm -f $BINDIR/gr-commits2tag.bkp
-install -m 0777 -v ./gr-pull $BINDIR
+$INSTALL_755 ./gr-pull $BINDIR
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/gr-pull && rm -f $BINDIR/gr-pull.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/gr-pull && rm -f $BINDIR/gr-pull.bkp
-install -m 0777 -v ./gr-pullreset $BINDIR
+$INSTALL_755 ./gr-pullreset $BINDIR
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/gr-pullreset && rm -f $BINDIR/gr-pullreset.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/gr-pullreset && rm -f $BINDIR/gr-pullreset.bkp
-install -m 0777 -v ./gr-remotes $BINDIR
+$INSTALL_755 ./gr-remotes $BINDIR
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/gr-remotes && rm -f $BINDIR/gr-remotes.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/gr-remotes && rm -f $BINDIR/gr-remotes.bkp
-install -m 0777 -v ./gr-show $BINDIR
+$INSTALL_755 ./gr-show $BINDIR
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/gr-show  && rm -f $BINDIR/gr-show.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/gr-show  && rm -f $BINDIR/gr-show.bkp
-install -m 0777 -v ./gr-showlocal $BINDIR
+$INSTALL_755 ./gr-showlocal $BINDIR
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/gr-showlocal  && rm -f $BINDIR/gr-showlocal.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/gr-showlocal  && rm -f $BINDIR/gr-showlocal.bkp
-install -m 0777 -v ./gr-initbare $BINDIR
+$INSTALL_755 ./gr-initbare $BINDIR
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/gr-initbare  && rm -f $BINDIR/gr-initbare.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/gr-initbare  && rm -f $BINDIR/gr-initbare.bkp
-install -m 0777 -v ./gr-tags $BINDIR
+$INSTALL_755 ./gr-tags $BINDIR
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/gr-tags  && rm -f $BINDIR/gr-tags.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/gr-tags  && rm -f $BINDIR/gr-tags.bkp
-install -m 0777 -v ./gr-changes $BINDIR
+$INSTALL_755 ./gr-changes $BINDIR
 sed -i".bkp" "1,/^VERSION=/s/^VERSION=.*/VERSION=$APP_FULL_VERSION_TAG/" $BINDIR/gr-changes  && rm -f $BINDIR/gr-changes.bkp
 sed -i".bkp" "1,/^VERSION_DATE=/s/^VERSION_DATE=.*/VERSION_DATE=$APP_BUILD_DATE/" $BINDIR/gr-changes  && rm -f $BINDIR/gr-changes.bkp
 
 mkdir -p -m 0755 $COMPLETION_DIR
-install -m 0777 -v ./gr-scripts_completion  $COMPLETION_DIR/
+$INSTALL_755 ./gr-scripts_completion  $COMPLETION_DIR/
 
 MANPAGES=`find ./doc/manpages -type f`
-install -d -m 755 $MANDIR/man1
-install -m 644 $MANPAGES $MANDIR/man1
+mkdir -p -m 755 $MANDIR/man1
+$INSTALL_644 $MANPAGES $MANDIR/man1
 
